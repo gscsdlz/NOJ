5e74eef985ed61e5b857ef8b1f411c14abe380b2
gscsdlz <lz842063523@foxmail.com>
2017-06-01 19:57:25 +0800

    准备支持OI模式

--- Model/statusModel.php
+++ Model/statusModel.php
@@ -5,7 +5,7 @@ class statusModel extends DB {
 		parent::__construct ();
 	}
 	public function getStatus($submit_id, $pro_id, $username, $lang, $status, $start, $end, $contest_id = 0) {
-		$q = "SELECT status.submit_id, submit_time, status.pro_id, run_time, run_memory, codes.code_length, lang, status.status, username FROM";
+		$q = "SELECT status.submit_id, submit_time, status.pro_id, run_time, run_memory, codes.code_length, lang, status.status, username, score FROM";
 		$q .= " status LEFT JOIN codes ON (status.submit_id = codes.submit_id), users WHERE users.user_id = status.user_id AND contest_id = $contest_id ";
 		if ($submit_id)
 			$q .= "AND status.submit_id = $submit_id ";
@@ -30,9 +30,15 @@ class statusModel extends DB {
 		if ($result->rowCount () != 0) {
 			while ( $row = $result->fetch ( PDO::FETCH_NUM ) ) {
 				if($contest_id){
-					$row[2] =  $this->get_inner_id($row[2], $contest_id);
-					
-				} //更正比赛模式下 真实ID和虚拟ID
+                    $row[2] =  $this->get_inner_id($row[2], $contest_id);
+					if($row[9] != -1) {
+					    $row[9] = $this->get_max_score($row[9], $contest_id, $row[2]);
+                    } else {
+					    unset($row[9]);
+                    }
+				} else {//更正比赛模式下 真实ID和虚拟ID
+                    unset($row[9]); //普通模式不启用OI
+                }
 				$arr [] = $row;
 			}
 			return $arr;
@@ -69,4 +75,9 @@ class statusModel extends DB {
 			return -1;
 		}
 	}
+
+	public function get_max_score($rate, $inner_id, $cid) {
+	    $res = parent::query_one("SELECT max_score FROM contest_pro WHERE contest_id = ? AND inner_id = ?", [$inner_id, $cid]);
+	    return $res[0] * $rate / 100;
+    }
 }
