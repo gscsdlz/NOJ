e735959e9eb72cafb9fdcbcbc62b281e596e1558
gscsdlz <lz842063523@foxmail.com>
2017-06-06 14:58:38 +0800

    update

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$stime = microtime ( true );
+//$stime = microtime ( true );
 date_default_timezone_set ( "PRC" );
 define ( "APPPATH", dirname ( __FILE__ ) );
 require APPPATH . '/Config/config.php';
@@ -8,6 +8,7 @@ require APPPATH . '/Include/WORK.class.php';
 require APPPATH . '/Include/DB.class.php';
 require APPPATH . '/Include/function.php';
 require APPPATH . '/Include/redisDB.class.php';
+
 global $redisInfo;
 extract ( $redisInfo );
 ini_set("session.save_handler", "redis");
@@ -19,20 +20,15 @@ $router = new router ();
 $controlClass = $router->control;
 $action = $router->action;
 new redisDB (); // redis只包含一个类静态成员 直接实例化一次就好
-if (reload_check ()) {
-	require APPPATH . '/View/VIEW.class.php';
-	VIEW::show ( 'error', array (
-			'errorInfo' => 'refresh Error' 
-	) );
+
+if (file_exists ( APPPATH . '/Include/' . $controlClass . '.php' )) {
+	require APPPATH . '/Include/' . $controlClass . '.php';
+	$control = WORK::create ( $controlClass );
+	$control->$action ();
 } else {
-	if (file_exists ( APPPATH . '/Include/' . $controlClass . '.php' )) {
-		require APPPATH . '/Include/' . $controlClass . '.php';
-		$control = WORK::create ( $controlClass );
-		$control->$action ();
-	} else {
-		require APPPATH . '/Include/indexControl.php';
-		$control = new indexControl ();
-		$control->index ();
-	}
+	require APPPATH . '/Include/indexControl.php';
+	$control = new indexControl ();
+	$control->index ();
 }
+
 ?>
